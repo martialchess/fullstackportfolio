69bd64294fc7791dcdb055806d6d5841ef936026
Rida Malik <internalprogression@gmail.com>
2025-04-01 17:00:20 +0500

    updated contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -12,7 +12,7 @@ export default function ContactForm() {
     e.preventDefault();
     setStatus('Sending...');
 
-    const res = await fetch('http://localhost:5000/api/contact', {
+    const res = await fetch('https://fullstackbackend-ub4b.onrender.com/api/contact', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form),
