a629345f6fb4ad225e22eee7bbbb316a3b8e33b0
Rida Malik <internalprogression@gmail.com>
2025-04-02 18:59:16 +0500

    fixed margin top of about

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 export default function AboutSection() {
     return (
-      <section className="bg-white p-6 mt-40 rounded-2xl shadow-md">
+      <section className="bg-white p-6 mt-16 rounded-2xl shadow-md">
         <h2 className="text-3xl font-bold text-blue-800 mb-4">About Me</h2>
         <p className="text-gray-700 mb-4">
           I am a passionate developer with a love for creating dynamic and responsive web applications. I have a strong foundation in JavaScript, React, and Node.js, and Im always eager to learn new technologies and improve my skills.
