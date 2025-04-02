da307ab4bee6c3f8bf9ee4d130481227b65961f7
Rida Malik <internalprogression@gmail.com>
2025-04-02 17:49:13 +0500

    reinstalled dependencies

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  };
-  
